9b4b9dbcfce14f3f94ddc9c2ce2432c90e6dc751
Aliitas <eugerijo@gmail.com>
2026-08-31 10:17:13 -0400

    feat(ui): interfaz multipagina con catalogo, elaboracion, historico y simulador

---
+++ tests/unit/test_ui_importable.py
@@ -0,0 +1,13 @@
+import importlib
+
+
+def test_las_paginas_se_importan_sin_efectos():
+    for mod in (
+        "ui.app",
+        "ui.paginas.actualizacion",
+        "ui.paginas.catalogo",
+        "ui.paginas.elaborar",
+        "ui.paginas.historico",
+        "ui.paginas.simulador",
+    ):
+        importlib.import_module(mod)
